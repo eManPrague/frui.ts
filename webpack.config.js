43b547536bc0987886ad339777af5367a88d2f3d
Augustin Šulc <sulc.gusta@gmail.com>
2019-02-10 15:44:52 +0100

    Fixed build configuration

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/controls/testControl.tsx',
+  entry: './src/index.ts',
   devtool: 'inline-source-map',
   module: {
     rules: [
