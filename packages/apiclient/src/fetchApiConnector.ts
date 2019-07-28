13666de45884df76cfd4befc3949d4c99644d635
Augustin Šulc <sulc.gusta@gmail.com>
2019-07-27 23:23:24 -0500

    Error handling in fetch made customizable

--- packages/apiclient/src/fetchApiConnector.ts
+++ packages/apiclient/src/fetchApiConnector.ts
@@ -3,56 +3,56 @@ import FetchError from "./FetchError";
 import { IApiConnector } from "./types";
 
 export class FetchApiConnector implements IApiConnector {
-  constructor(private fetchFunction = bind(window.fetch, window)) {}
+  constructor(private fetchFunction = bind(window.fetch, window), private handleError = defaultHandleError) {}
 
   getText(url: string, params?: RequestInit) {
     return this.fetchFunction(url, { ...params, method: "get" })
-      .then(checkStatus)
+      .then(this.handleError)
       .then(x => x.text());
   }
   getJson<T>(url: string, params?: RequestInit): Promise<T> {
     return this.fetchFunction(url, { ...params, method: "get" })
-      .then(checkStatus)
+      .then(this.handleError)
       .then(x => x.json());
   }
   getBlob(url: string, params?: RequestInit) {
     return this.fetchFunction(url, { ...params, method: "get" })
-      .then(checkStatus)
+      .then(this.handleError)
       .then(x => x.blob());
   }
   postText(url: string, text: string, params?: RequestInit): Promise<Response> {
-    return this.fetchFunction(url, { ...params, method: "post", body: text }).then(checkStatus);
+    return this.fetchFunction(url, { ...params, method: "post", body: text }).then(this.handleError);
   }
   postJson<TResult>(url: string, content: any, params?: RequestInit): Promise<TResult> {
     return this.postText(url, JSON.stringify(content), params).then(x => x.json());
   }
   postFormData(url: string, data: FormData, params?: RequestInit): Promise<Response> {
-    return this.fetchFunction(url, { ...params, method: "post", body: data }).then(checkStatus);
+    return this.fetchFunction(url, { ...params, method: "post", body: data }).then(this.handleError);
   }
   putJson<TResult>(url: string, content: any, params?: RequestInit): Promise<TResult> {
     return this.putText(url, JSON.stringify(content), params).then(x => x.json());
   }
   putText(url: string, text: string, params?: RequestInit): Promise<Response> {
-    return this.fetchFunction(url, { ...params, method: "put", body: text }).then(checkStatus);
+    return this.fetchFunction(url, { ...params, method: "put", body: text }).then(this.handleError);
   }
   putFormData(url: string, data: FormData, params?: RequestInit): Promise<Response> {
-    return this.fetchFunction(url, { ...params, method: "put", body: data }).then(checkStatus);
+    return this.fetchFunction(url, { ...params, method: "put", body: data }).then(this.handleError);
   }
   delete(url: string, params?: RequestInit): Promise<Response> {
-    return this.fetchFunction(url, { ...params, method: "delete" }).then(checkStatus);
+    return this.fetchFunction(url, { ...params, method: "delete" }).then(this.handleError);
   }
 }
 
-async function checkStatus(response: Response) {
+async function defaultHandleError(response: Response) {
   if (response.status >= 200 && response.status < 300) {
     return response;
   }
-  if (response.json) {
-    try {
-      const content = await response.json();
-      throw new FetchError(response, content);
-    } catch {}
-  }
 
-  throw new FetchError(response);
+  let content: any;
+  try {
+    content = await response.json();
+  } catch {
+    throw new FetchError(response);
+  }
+  throw new FetchError(response, content);
 }
