10bba17a83d9c0da1d0b23675ec7e2f39c9d0af4
Augustin Šulc <sulc.gusta@gmail.com>
2019-09-01 08:42:16 +0200

    Fixed missing export

--- packages/datascreens/src/index.ts
+++ packages/datascreens/src/index.ts
@@ -1,2 +1,3 @@
+export { default as DetailViewModel } from "./detailViewModel";
 export { default as FilteredListViewModel } from "./filteredListViewModel";
 export { default as ListViewModel } from "./listViewModel";
