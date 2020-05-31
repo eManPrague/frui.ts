cacc87f118565a66e67579070fb8186820e097b5
Augustin Šulc <sulc.gusta@gmail.com>
2020-05-31 15:42:25 +0200

    Peer dependencies are installed during initialization

---
+++ lerna-bootstrap-fix.cmd
@@ -0,0 +1,16 @@
+call yarn lerna bootstrap --scope=@frui.ts/apiclient --force-local
+call yarn lerna bootstrap --scope=@frui.ts/bootstrap --force-local
+call yarn lerna bootstrap --scope=@frui.ts/cra-template --force-local
+call yarn lerna bootstrap --scope=@frui.ts/data --force-local
+call yarn lerna bootstrap --scope=@frui.ts/datascreens --force-local
+call yarn lerna bootstrap --scope=@frui.ts/dataviews --force-local
+call yarn lerna bootstrap --scope=@frui.ts/dirtycheck --force-local
+call yarn lerna bootstrap --scope=@frui.ts/generator --force-local
+call yarn lerna bootstrap --scope=@frui.ts/helpers --force-local
+call yarn lerna bootstrap --scope=@frui.ts/htmlcontrols --force-local
+call yarn lerna bootstrap --scope=@frui.ts/screens --force-local
+call yarn lerna bootstrap --scope=@frui.ts/validation --force-local
+call yarn lerna bootstrap --scope=@frui.ts/views --force-local
+call yarn lerna bootstrap --scope=@frui.ts/example-complexdemo --force-local
+call yarn lerna bootstrap --scope=@frui.ts/examples-redminedemo --force-local
+call yarn lerna bootstrap --scope=@frui.ts/examples-simpletodolist --force-local
