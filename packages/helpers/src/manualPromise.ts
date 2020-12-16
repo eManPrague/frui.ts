f987c4d9afbc256acc1866a202270e3492dd2b2d
Jan Strnádek <jan.strnadek@eman.cz>
2020-12-16 07:19:45 +0100

    Allow to check status of rejected or resolved ManualPromise

--- packages/helpers/src/manualPromise.ts
+++ packages/helpers/src/manualPromise.ts
@@ -2,11 +2,20 @@ export default class ManualPromise<T = any> {
   promise: Promise<T>;
   resolve: (result?: T | PromiseLike<T>) => void;
   reject: (reason: any) => void;
+  isResolved = false;
+  isRejected = false;
 
   constructor() {
     this.promise = new Promise<T>((resolve, reject) => {
-      this.resolve = resolve;
-      this.reject = reject;
+      this.resolve = (result: any) => {
+        this.isResolved = true;
+        resolve(result);
+      };
+
+      this.reject = (reason: any) => {
+        this.isRejected = true;
+        reject(reason);
+      };
     });
   }
 }
