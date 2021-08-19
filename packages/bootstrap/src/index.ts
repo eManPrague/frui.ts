c53a569ee245abac24cf8d99cdb9cf93f5e75363
Michal Kopecký <michal.kopeck@eman.cz>
2021-08-19 10:25:29 +0200

    #44 Remove label control

--- packages/bootstrap/src/index.ts
+++ packages/bootstrap/src/index.ts
@@ -3,6 +3,5 @@ export * from "./controls/collectionCheck";
 export * from "./controls/dropdownSelect";
 export { Input } from "./controls/input";
 export { Select } from "./controls/select";
-export { Label } from "./controls/label";
 export { ValidationControlBase } from "./controls/validationControlBase";
 export { ValidationError } from "./controls/validationError";
