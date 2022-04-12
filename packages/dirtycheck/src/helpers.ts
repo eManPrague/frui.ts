d6e573fe4bd097a0ea56676f42b49a34b7a5bb5e
Michal Kopecký <michal.kopecky@eman.cz>
2022-04-12 10:32:22 +0200

    #65 Allow specifying DirtyWatch config via attachAutomaticDirtyWatcher

--- packages/dirtycheck/src/helpers.ts
+++ packages/dirtycheck/src/helpers.ts
@@ -1,6 +1,6 @@
 import { PropertyName } from "@frui.ts/helpers";
 import { get, runInAction } from "mobx";
-import AutomaticDirtyWatcher from "./automaticDirtyWatcher";
+import AutomaticDirtyWatcher, { DirtyWatchConfig } from "./automaticDirtyWatcher";
 import ManualDirtyWatcher from "./manualDirtyWatcher";
 import { IHasDirtyWatcher, IHasManualDirtyWatcher } from "./types";
 
@@ -8,9 +8,13 @@ import { IHasDirtyWatcher, IHasManualDirtyWatcher } from "./types";
  * Attaches a new [[AutomaticDirtyWatcher]] to the entity and returns the entity typed as [[IHasDirtyWatcher]]
  * @returns The target entity instance with `IHasDirtyWatcher` implemented with the attached watcher
  */
-export function attachAutomaticDirtyWatcher<TTarget>(target: TTarget, dirtyFlagsImmediatelyVisible = false) {
+export function attachAutomaticDirtyWatcher<TTarget>(
+  target: TTarget,
+  dirtyFlagsImmediatelyVisible = false,
+  dirtyWatchConfig?: DirtyWatchConfig<TTarget>
+) {
   const typedTarget = target as TTarget & IHasDirtyWatcher<TTarget>;
-  typedTarget.__dirtycheck = new AutomaticDirtyWatcher(target, dirtyFlagsImmediatelyVisible);
+  typedTarget.__dirtycheck = new AutomaticDirtyWatcher(target, dirtyFlagsImmediatelyVisible, dirtyWatchConfig);
   return typedTarget;
 }
 
